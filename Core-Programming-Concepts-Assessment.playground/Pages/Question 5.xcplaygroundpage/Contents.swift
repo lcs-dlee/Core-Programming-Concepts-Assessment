635e056033b1c14407168513414f2fe76cf35576
Lee, Dakyum <dlee@lcs-mba3088.lcs.on.ca>
2018-01-26 09:55:49 -0500

    Answer Question 5

--- Core-Programming-Concepts-Assessment.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
+++ Core-Programming-Concepts-Assessment.playground/Pages/Question 5.xcplaygroundpage/Contents.swift
@@ -24,6 +24,12 @@ let temperatureInCelsius = -6
 let isSnowMakingEquipmentBroken = true
 
 // Answer question 5 below
+let skiHillOpenDay = newSnowfallAmount > 10 || temperatureInCelsius < -5 && !isSnowMakingEquipmentBroken
+if skiHillOpenDay {
+    print("We're open!")
+} else {
+    print("We're closed.")
+}
 
 
 /*:
